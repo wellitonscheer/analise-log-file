34e457ae30b13347816cd443f3e12b8f94d800c2
caie <wellitonscheer@gmail.com>
2026-02-13 09:30:14 -0300

    define lines to split

--- cmd/split.go
+++ cmd/split.go
@@ -1,44 +1,47 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/wellitonscheer/analise-log-file/utils"
 )
 
 var splitCmd = &cobra.Command{
 	Use:   "split",
 	Short: "split lines and print results",
 	Run: func(cmd *cobra.Command, args []string) {
-		lines, err := cmd.Flags().GetInt64("rows")
+		lines, err := cmd.Flags().GetInt("lines")
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Printf("%d\n", lines)
+
+		file, err := os.Open("logs/EDLOGFILEREGISTROS_202602100903.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		for i := 0; i < lines; i++ {
+			scanner.Scan()
+			line := scanner.Text()
+			split := utils.SplitAtPipe(line)
+			fmt.Printf("\n%#+v\n", split)
+		}
+
+		if err = scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
 	},
 }
-
-// file, err := os.Open("logs/EDLOGFILEREGISTROS_202602100903.txt")
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// defer file.Close()
-
-// scanner := bufio.NewScanner(file)
-
-// for scanner.Scan() {
-// 	line := scanner.Text()
-// 	split := utils.SplitAtPipe(line)
-// 	fmt.Printf("\n%#+v\n", split)
-// 	break
-// }
-
-// if err = scanner.Err(); err != nil {
-// 	log.Fatal(err)
-// }
 
 func init() {
 	rootCmd.AddCommand(splitCmd)
 
-	splitCmd.Flags().Int64P("lines", "l", 10, "Number of lines to split")
+	splitCmd.Flags().IntP("lines", "l", 10, "Number of lines to split")
 }
